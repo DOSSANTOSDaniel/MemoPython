7014b6789172586996014c880831839fbf2acb4f
DOSSANTOSDaniel <daniel.massy91@gmail.com>
2020-10-23 13:11:26 +0200

    Création de la fonction modifyFile

--- TPs/WRfile.py
+++ TPs/WRfile.py
@@ -24,13 +24,13 @@ import os
 def writeFile(myFile):
     f = open(myFile, "a")
     while True:
-        ligne = input("Ecrivez ici, laisser une ligne vide pour quitter")
+        ligne = input("Ecrivez ici, laisser une ligne vide pour quitter : ")
         if ligne == '':
             break
         f.write(ligne + "\n")
     f.close()
 
-def readFile():
+def readFile(myFile):
     f = open(myFile, "r")
     texte = f.read()
     print(texte)
@@ -45,7 +45,36 @@ def removeFile(myFile):
     os.remove(myFile)
     print("\n Fichier supprimé ! \n")
 
-# def modifyFile():
+def modifyFile(myFile):
+    nbLines = 0
+
+    f = open(myFile, 'r')
+    listeLines = f.readlines()
+    f.close()
+
+    for i in listeLines:
+        nbLines = int(nbLines) + 1
+        print("Ligne :", nbLines, ">", i)
+
+    print()
+    changeLine = input("Numéro de ligne à changer : ")
+    changeLine = int(changeLine) - 1
+
+    newLine = input("Ecrire dans la nouvelle ligne : ")
+
+    listeLines.remove(listeLines[changeLine])
+    listeLines.insert(changeLine, newLine + "\n")
+
+    f = open(myFile, "w")
+    f.truncate()
+    f.close()
+
+    f = open(myFile, "a")
+    f.writelines(listeLines)
+    f.close()
+
+    print("\n Sauvegarde effectué \n")
+
 
 # Programme
 # --------------------
@@ -60,21 +89,28 @@ while True:
     print("| Saisir [5] pour supprimer un fichier texte |")
     print("+--------------------------------------------+ \n")
 
-    chMenu = input("Votre choix : ")
-    print()
+    chMenu = '0'
+    while chMenu not in ['1', '2', '3', '4', '5']:
+        chMenu = input("Votre choix : ")
+        print()
 
     FileDoc = input("Sur quel fichier voulez-vous travailler ? : ")
     print()
 
-    while chMenu not in ['1', '2', '3', '4', '5']:
-        print("\n Erreur de saissie \n")
-        chMenu = input("Votre choix : ")
-        print()
-
     if chMenu == '1':
-        print("Création d'un fichier")
+        print("\n Création d'un fichier \n")
         createFile(FileDoc)
+    elif chMenu == '2':
+        print("\n Mode écriture \n")
+        writeFile(FileDoc)
+    elif chMenu == '3':
+        print("\n Mode modification \n")
+        modifyFile(FileDoc)
+    elif chMenu == '4':
+        print("\n Mode lecture \n")
+        readFile(FileDoc)
+    elif chMenu == '5':
+        print("\n Suppression d'un fichier \n")
+        removeFile(FileDoc)
     else:
-        pass
-
-print("FIN")
+        print("\n Erreur de saissie ! \n")
